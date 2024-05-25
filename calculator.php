33e5c35693246e1dddab100511478334be0295cb
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2024-05-25 10:19:53 +0300

    go

--- calculator.php
+++ calculator.php
@@ -14,10 +14,10 @@ get_header()
 
             <div class="calculator-data-form">
                 <label id="handedness-label" for="handedness">Оберіть домінуючу руку:
-                <select id="handedness" name="handedness">
-                    <option value="right">Правша</option>
-                    <option value="left">Лівша</option>
-                </select></label>
+                    <select id="handedness" name="handedness">
+                        <option value="right">Правша</option>
+                        <option value="left">Лівша</option>
+                    </select></label>
                 <label class="calculator-date-label" for="birthday">
                     Введіть дату народження:
 
@@ -120,7 +120,7 @@ get_header()
                 </div>
                 <div class="calculator-response-two-columns">
                     <div class="calculator-response-contour-of-specialness">
-                            <h3 class="calculator-response-contour-of-specialness-title">Контури</h3>
+                        <h3 class="calculator-response-contour-of-specialness-title">Контури</h3>
                         <ul class="calculator-response-contour-of-specialness-list">
                             <li class="calculator-response-contour-of-specialness-item">
                                 <div class="calculator-response-contour-of-specialness-item-title">
@@ -149,26 +149,26 @@ get_header()
                         </ul>
                     </div>
                     <div class="calculator-response-typology-of-specialness">
-                            <div class="calculator-response-typology-of-specialness-descr">
-                                3-а типологія
-                            </div>
+                        <div class="calculator-response-typology-of-specialness-descr">
+                            3-а типологія
+                        </div>
 
                     </div>
                     <div class="calculator-response-womans-and-mans-cob">
                         <h3 class="calculator-response-womans-and-mans-title">
                             Жіноча та чоловіча енергія
                         </h3>
-                            <div class="calculator-response-womans-and-mans-cob-con">
-                                <div class="calculator-response-womans-and-mans-cob-in">
-                                    Інь: 121
-                                </div>
-                                <div class="calculator-response-womans-and-mans-cob-an">
-                                    Ян: 146
-                                </div>
+                        <div class="calculator-response-womans-and-mans-cob-con">
+                            <div class="calculator-response-womans-and-mans-cob-in">
+                                Інь: 121
                             </div>
+                            <div class="calculator-response-womans-and-mans-cob-an">
+                                Ян: 146
+                            </div>
+                        </div>
                     </div>
                 </div>
-        </div>
+            </div>
             <div class="calculator-response-columns-2">
                 <div class="calculator-response-the-power-of-energy">
                     <h3 class="calculator-response-the-power-of-energy-title">
@@ -183,7 +183,7 @@ get_header()
                         </div>
                     </div>
                     <div class="sexual-potential">
-                         Енергетичний потенціал
+                        Енергетичний потенціал
                         <span>875</span>
                     </div>
                 </div>
@@ -203,82 +203,163 @@ get_header()
                         </div>
                     </div>
                 </div>
+                <div class="calculator-response-emotional-type">
+                    <div class="calculator-response-emotional-type-container">
+                        <div class="calculator-response-emotional-type-1 calculator-response-emotional-types">
+                            Iнтуїція: 48
+                        </div>
+                        <div class="calculator-response-emotional-type-2 calculator-response-emotional-types">
+                            Логіка: 39,6
+                        </div>
+                    </div>
+                </div>
             </div>
-        <div class="calculator-response-periods-of-life">
-            <h3 class="calculator-response-periods-of-life-title">
-                Періоди життя
-            </h3>
-            <ul class="calculator-response-periods-of-life-list">
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        до 12 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        0
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        6
-                    </div>
-                </li>
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        12-24 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        2
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        8
-                    </div>
-                </li>
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        24-36 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        2
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        8
-                    </div>
-                </li>
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        36-48 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        2
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        8
-                    </div>
-                </li>
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        48-60 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        2
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        8
-                    </div>
-                </li>
-                <li class="calculator-response-periods-of-life-item">
-                    <div class="calculator-response-periods-of-life-item-periods">
-                        більше 60 років
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-1">
-                        2
-                    </div>
-                    <div class="calculator-response-periods-of-life-item-num-2">
-                        8
-                    </div>
-                </li>
-            </ul>
+            <div class="last-position">
+                <ul class="live-tasks">
+                    <li class="live-task">
+                        <div class="live-task-name">
+                            Родова
+                        </div>
+                        <div class="live-task-descr">
+                            test
+                        </div>
+                    </li>
+                    <li class="live-task">
+                        <div class="live-task-name">
+                            Карма минулого
+                        </div>
+                        <div class="live-task-descr">
+                            test
+                        </div>
+                    </li>
+                    <li class="live-task">
+                        <div class="live-task-name">
+                            Теперішня
+                        </div>
+                        <div class="live-task-descr">
+                            test
+                        </div>
+                    </li>
+                    <li class="live-task">
+                        <div class="live-task-name">
+                            Батьківська
+                        </div>
+                        <div class="live-task-descr">
+                            test
+                        </div>
+                    </li>
 
+                </ul>
+                    <ul class="live-tasks">
+                        <li class="live-task">
+                            <div class="live-task-name">
+                                Потенціал душі
+                            </div>
+                            <div class="live-task-descr">
+                                test
+                            </div>
+                        </li>
+                        <li class="live-task">
+                            <div class="live-task-name">
+                                Втілення душі
+                            </div>
+                            <div class="live-task-descr">
+                                test
+                            </div>
+                        </li>
+                        <li class="live-task">
+                            <div class="live-task-name">
+                                Фін код
+                            </div>
+                            <div class="live-task-descr">
+                                test
+                            </div>
+                        </li>
+                        <li class="live-task">
+                            <div class="live-task-name">
+                                Прогноз
+                            </div>
+                            <div class="live-task-descr">
+                                test
+                            </div>
+                        </li>
+                    </ul>
+            </div>
+
+            <div class="calculator-response-periods-of-life">
+                <h3 class="calculator-response-periods-of-life-title">
+                    Періоди життя
+                </h3>
+                <ul class="calculator-response-periods-of-life-list">
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            до 12 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            0
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            6
+                        </div>
+                    </li>
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            12-24 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            2
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            8
+                        </div>
+                    </li>
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            24-36 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            2
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            8
+                        </div>
+                    </li>
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            36-48 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            2
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            8
+                        </div>
+                    </li>
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            48-60 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            2
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            8
+                        </div>
+                    </li>
+                    <li class="calculator-response-periods-of-life-item">
+                        <div class="calculator-response-periods-of-life-item-periods">
+                            більше 60 років
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-1">
+                            2
+                        </div>
+                        <div class="calculator-response-periods-of-life-item-num-2">
+                            8
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
     </div>
 </section>
 
